b2f4396139af949604f39ea09c3a1dd73521c14d
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2023-07-15 21:25:14 -0300

    ajuste conexão ao banco

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,12 @@
 import { DataSourceOptions } from "typeorm";
 
 export const ormConfig: DataSourceOptions = {
-  "database": "crud_carro",
-  "host": "localhost",
-  "type": "mariadb",
+  "database": "blpvdpisjtnfbb6c13jk", // "crud_carro",
+  "host": "blpvdpisjtnfbb6c13jk-mysql.services.clever-cloud.com", // "localhost",
+  "type": "mysql",
   "port": 3306,
-  "username": "g3_informatica",
-  "password": "#g31nf#",
-  "synchronize": true,
+  "username": "uzozut4jzpeqdwkk",//"g3_informatica",
+  "password": "4t5mhh2G05ssbNkCm08f", //"#g31nf#",
+  //"synchronize": true,
   "entities": ["dist/**/*.model.js"]
 }
